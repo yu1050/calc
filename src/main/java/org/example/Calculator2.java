1972383710afb1d43f9a0e43a8f5aa0d954365ea
Frank <yu1050@gmail.com>
2024-02-16 14:58:06 -0500

    lexical

--- src/main/java/org/example/Calculator2.java
+++ src/main/java/org/example/Calculator2.java
@@ -1,6 +1,8 @@
 package org.example;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 
 public class Calculator2 {
@@ -9,7 +11,7 @@ public class Calculator2 {
         String input = scanner.nextLine();
         System.out.println(input);
 
-        String[] tokens = input.split(" ");
+        String[] tokens = lexicalAnalysis(input);
         System.out.println(Arrays.toString(tokens));
 
         int result = 0;
@@ -17,7 +19,7 @@ public class Calculator2 {
         for (int i = 0; i < tokens.length; i++) {
             String token = tokens[i];
 
-            if (token.equals("+") || token.equals("-") || token.equals("*") || token.equals("/")) {
+            if (isOperator(token)) {
                 operator = token;
                 continue;
             }
@@ -44,5 +46,32 @@ public class Calculator2 {
 
         System.out.println("Result: " + result);
         System.exit(0);
+    }
+
+    static String[] lexicalAnalysis(String input) {
+        List<String> list = new ArrayList<>();
+
+        String temporary = "";
+        for (int position = 0; position < input.length(); position++) {
+            char current = input.charAt(position);
+            if (Character.isDigit(current)) {
+                //
+            } else if (isOperator(Character.toString(current))) {
+                //
+            }
+        }
+//
+        String[] tokens = new String[list.size()];
+        for (int i = 0; i < list.size(); i++) {
+            tokens[i] = list.get(i);
+        }
+
+//        return tokens;
+
+        return input.split(" ");
+    }
+
+    static public boolean isOperator(String token) {
+        return (token.equals("+") || token.equals("-") || token.equals("*") || token.equals("/"));
     }
 }
