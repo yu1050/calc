5e4b175f1bac1803e2520f6a3a8a5f7b4ff730de
fuguo <fuguo@charless-imac.home>
2024-02-16 12:40:26 -0500

    add java implement

---
+++ src/main/java/org/example/Calculator1.java
@@ -0,0 +1,45 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class Calculator1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double num1, num2, result;
+        char operator;
+
+        System.out.print("Enter first number: ");
+        num1 = scanner.nextDouble();
+
+        System.out.print("Enter operator (+, -, *, /): ");
+        operator = scanner.next().charAt(0);
+
+        System.out.print("Enter second number: ");
+        num2 = scanner.nextDouble();
+
+        switch (operator) {
+            case '+':
+                result = num1 + num2;
+                break;
+            case '-':
+                result = num1 - num2;
+                break;
+            case '*':
+                result = num1 * num2;
+                break;
+            case '/':
+                if (num2 != 0) {
+                    result = num1 / num2;
+                } else {
+                    System.out.println("Error: Division by zero!");
+                    return;
+                }
+                break;
+            default:
+                System.out.println("Error: Invalid operator!");
+                return;
+        }
+
+        System.out.println("Result: " + result);
+    }
+}
